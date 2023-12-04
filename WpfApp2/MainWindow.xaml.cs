009e5ddc65bc71d25bfe606d05ab093eeb2fefea
BushevAndrey <chhufudud@gmail.com>
2023-12-04 14:46:59 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyFrame.Navigate(new Page1());
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyFrame.CanGoBack)
+            {
+                MyFrame.GoBack();
+            }
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyFrame.CanGoForward)
+            {
+                MyFrame.GoForward();
+            }
+        }
+
+        private void A_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new Page1());
+        }
+        private void B_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new Page2());
+        }
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new Page3());
+        }
+        private void D_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new Page4());
+        }
+        private void J_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new Page5());
+        }
+    }
+}
